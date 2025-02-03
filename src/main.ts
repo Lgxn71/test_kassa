1f7162e5b50e160344a40bcdfd9c442ca95a4f0b
Mukhammedali Akhmetbekuly <muhanai1913@gmail.com>
2025-02-03 22:44:53 +0500

    [chore]: added firebase to project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
 
 import { router } from "@/router/routes";
 
 import App from "@/App.vue";
 
 import "@/assets/index.css";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 app.use(createPinia());
