709ad9e169c56feab87d2473b3177e66494f0586
Mukhammedali Akhmetbekuly <muhanai1913@gmail.com>
2025-02-03 23:12:41 +0500

    [chore]: added font according to design in figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,6 +81,10 @@ module.exports = {
           to: { height: 0 },
         },
       },
+
+      fontFamily: {
+        sans: ["Mabry Pro", "sans-serif"],
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
